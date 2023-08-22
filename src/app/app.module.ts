c3d8891837ac0b9c6b584001c01ad4c6ceef5e98
nash99 <ignacio99k@hotmail.com>
2023-08-22 13:46:09 -0400

    en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common-components/navbar/navbar.component';
+import { MenuComponent } from './common-components/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import { MenuSelectComponent } from './common-components/menu-select/menu-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent,
+    MenuSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/common-components/menu-select/menu-select.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-menu-select',
+  templateUrl: './menu-select.component.html',
+  styleUrls: ['./menu-select.component.scss']
+})
+export class MenuSelectComponent {
+  @Input() name = '';
+  @Input() img = '';
+}
