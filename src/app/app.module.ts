2f2ec8f52431b7eef7d9e48459f0ee2410e65141
nash99 <ignacio99k@hotmail.com>
2023-08-29 19:09:04 -0400

    add: estilos, aun faltan resolver bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './common-components/navbar/navbar.component';
 import { MenuComponent } from './common-components/menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RegistrosComponent } from './registros/registros.component';
 
 
 import {MatButtonModule} from '@angular/material/button';
@@ -13,13 +15,30 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { MenuSelectComponent } from './common-components/menu-select/menu-select.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTableModule} from '@angular/material/table';
+
+//Enrutamiento
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+const appRoutes:Routes=[
+  {path:'home', component:HomeComponent},
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'registros', component:RegistrosComponent},
+  {path:'estadisticas', component:EstadisticasComponent},
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MenuComponent,
-    MenuSelectComponent
+    MenuSelectComponent,
+    HomeComponent,
+    RegistrosComponent,
+    PageNotFoundComponent,
+    EstadisticasComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +47,10 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatTableModule,
+
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common-components/menu-select/menu-select.component.ts
+++ src/app/common-components/menu-select/menu-select.component.ts
@@ -8,4 +8,5 @@ import { Component, Input} from '@angular/core';
 export class MenuSelectComponent {
   @Input() name = '';
   @Input() img = '';
+  @Input() page = '';
 }
